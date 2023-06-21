188e90c698e6c593831f81b5f62bc1048a50bac8
zaynalov005@mail.ru <zaynalov005@mail.ru>
2023-06-21 14:28:26 +0300

    three block

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ function next() {
   let img1 = document.querySelector(".slider-img-1");
   let img2 = document.querySelector(".slider-img-2");
   let img3 = document.querySelector(".slider-img-3");
+  let i1 = document.querySelector(".i1");
+  let i2 = document.querySelector(".i2");
+  let i3 = document.querySelector(".i3");
 
   if (i == 0) {
     img1.style.zIndex = "5"
@@ -14,6 +17,10 @@ function next() {
     img3.style.zIndex = "3"
     img3.style.marginLeft = "100px"
     img3.style.transform = 'scale(0.9)';
+    i1.classList.add('active')
+    i2.classList.remove('active')
+    i3.classList.remove('active')
+
     i += 1
   } else if (i == 1) {
     img1.style.zIndex = "4"
@@ -25,6 +32,9 @@ function next() {
     img3.style.zIndex = "4"
     img3.style.marginLeft = "50px"
     img3.style.transform = 'scale(0.95)';
+    i1.classList.remove('active')
+    i2.classList.add('active')
+    i3.classList.remove('active')
     i += 1
   } else {
     img1.style.zIndex = "4"
@@ -36,6 +46,9 @@ function next() {
     img3.style.zIndex = "5"
     img3.style.marginLeft = "0px"
     img3.style.transform = 'scale(1)';
+    i1.classList.remove('active')
+    i2.classList.remove('active')
+    i3.classList.add('active')
     i = 0
   }
 };
